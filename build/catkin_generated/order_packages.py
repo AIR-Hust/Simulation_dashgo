9b41ccab7bdcb5a508b0149a13a16d0c17668074
platon <dmatttqd@gmail.com>
2019-12-22 13:28:11 +0700

    initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/platon/dashgo_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/platon/simulation_ws/devel;/home/platon/dashgo_ws/devel;/home/platon/barista_ws/devel;/home/platon/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/platon/simulation_ws/devel;/home/platon/dashgo_ws/devel;/home/platon/barista_ws/devel;/home/platon/catkin_ws/devel;/opt/ros/kinetic" != "" else []
